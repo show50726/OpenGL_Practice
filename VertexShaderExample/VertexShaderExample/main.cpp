9de3b1c3a9f21ea0788f9fa5a4599bff52cd2c55
王小明 <k40526@hotmail.com>
2019-11-20 00:49:50 +0800

    update

--- VertexShaderExample/VertexShaderExample/main.cpp
+++ VertexShaderExample/VertexShaderExample/main.cpp
@@ -115,10 +115,7 @@ void shaderInit() {
 	glGenBuffers(1, &_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
 
-	VertexAttribute *_vertices;
-	_vertices = drawEarth(30, 30);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexAttribute) * verticeNumber, _vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexAttribute) * verticeNumber, vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(VertexAttribute), (void*)(offsetof(VertexAttribute, texcoord)));
 	glBindBuffer(GL_ARRAY_BUFFER, 1);
